ad627f20cf3a73c16f464452d9cbb6ce7bf36589
Hubert Pol <hubertpol@wp.pl>
2020-06-22 00:54:22 +0200

    add accordion

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+//const  { active } = require('browser-sync');
+
 {
   'use strict';
 
@@ -53,16 +55,15 @@
   };
 
   class Product {
-    constructor(id, data){
+    constructor(id, data) {
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
-      
       thisProduct.renderInMenu();
-      
-      console.log('new Product', thisProduct);
+      thisProduct.initAccordion();
+      //console.log('new Product', thisProduct);
     }
-  
+
     renderInMenu() {
       const thisProduct = this;
       /* generate HTML based on template */
@@ -74,25 +75,53 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
-}
+
+    initAccordion() {
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function() {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        /* find all active products */
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        
+        /* START LOOP: for each active product */
+        for (let activeProduct of activeProducts) {
+          /* START: if the active product isn't the element of thisProduct */
+          if (activeProduct != thisProduct.element) {
+            /* remove class active for the active product */
+            activeProduct.classList.remove('active');
+          /* END: if the active product isn't the element of thisProduct */
+          }
+        /* END LOOP: for each active product */
+        }
+        /* END: click event listener to trigger */
+      });
+    }
+    
+  }
 
   const app = {
-    initMenu: function(){
+    initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
-      
-      for(let productData in thisApp.data.products){
+      //console.log('thisApp.data:', thisApp.data);
+
+      for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
     },
 
-    initData: function(){
+    initData: function () {
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
 
-    init: function(){
+    init: function () {
       const thisApp = this;
       /*console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
