a9f6481f0273596fc338adb8ccd599b94ac66b81
Hubert Pol <hubertpol@wp.pl>
2020-07-07 18:58:28 +0200

    add class booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
-import {settings, select, classNames} from './settings.js';
+import {
+  settings,
+  select,
+  classNames
+} from './settings.js';
 import Product from './components/Product.js';
-import Cart from  './components/Cart.js';
+import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = { //obiekt który pomoże nam w organizacji kodu naszej aplikacji,
-  initPages: function(){
+  initPages: function () {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
@@ -13,21 +18,21 @@ const app = { //obiekt który pomoże nam w organizacji kodu naszej aplikacji,
 
     let pageMatchingHash = thisApp.pages[0].id;
 
-    for(let page of thisApp.pages){
-      if(page.id == idFromHash){
-       pageMatchingHash = page.id;
-       break; 
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
       }
     }
 
     thisApp.activatePage(idFromHash);
 
-    for(let link of thisApp.navLinks){
-      link.addEventListener('click', function(event){
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
         const clickedElement = this;
         event.preventDefault();
 
-        /* get page id from href attribute */ 
+        /* get page id from href attribute */
         const id = clickedElement.getAttribute('href').replace('#', '');
         /* run thisApp.activatePage with that id */
         thisApp.activatePage(id);
@@ -35,30 +40,30 @@ const app = { //obiekt który pomoże nam w organizacji kodu naszej aplikacji,
         window.location.hash = '#/' + id;
       });
     }
-    
+
 
   },
-  activatePage: function(pageId){
+  activatePage: function (pageId) {
     const thisApp = this;
 
     /* add class "active" to matching pages, remove from non-matching */
-    for(let page of thisApp.pages){
+    for (let page of thisApp.pages) {
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
     /* add class "active" to matching links, remove from non-matching */
-    for(let link of thisApp.navLinks){
+    for (let link of thisApp.navLinks) {
       link.classList.toggle(
-        classNames.nav.active, 
+        classNames.nav.active,
         link.getAttribute('href') == '#' + pageId
-        );
+      );
     }
-    
+
   },
 
-  initBooking: function(){
+  initBooking: function () {
     const thisApp = this;
-    thisApp.booking = document.querySelector(select.containerOf.booking);
-    
+    thisApp.bookingTab = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(thisApp.bookingTab);
   },
 
   initMenu: function () {
@@ -79,7 +84,7 @@ const app = { //obiekt który pomoże nam w organizacji kodu naszej aplikacji,
       .then(function (rawResponse) {
         return rawResponse.json();
       })
-      .then (function (parsedResponse) {
+      .then(function (parsedResponse) {
         console.log('parsedResponse', parsedResponse);
 
         /* save parsedResponse as thisApp.data.products */
@@ -98,7 +103,7 @@ const app = { //obiekt który pomoże nam w organizacji kodu naszej aplikacji,
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function(event){
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
   },
@@ -113,9 +118,8 @@ const app = { //obiekt który pomoże nam w organizacji kodu naszej aplikacji,
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
 app.init(); //wywołanie metody, która będzie uruchamiać wszystkie pozostałe komponenty strony.
-
-
---
+++ src/js/components/Booking.js
@@ -0,0 +1,36 @@
+import {
+  templates,
+  select
+} from '../settings.js';
+import {
+  utils
+} from '../utils.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(elemBooking) {
+    const thisBooking = this;
+    thisBooking.render(elemBooking);
+    thisBooking.initWidgets();
+
+  }
+  render(elemBooking) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = elemBooking;
+    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+    thisBooking.dom.wrapper.appendChild(generatedDOM);
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
+
+}
+
+export default Booking;
